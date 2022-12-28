e46813edb1743bb7e01f62582395a1000b7dcc2b
朱建业 <borisdunk@sina.com>
2022-12-28 11:06:48 +0800

    init config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,83 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv, ConfigEnv, UserConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+import { wrapperEnv } from './src/utils/getEnv'
+import { createHtmlPlugin } from 'vite-plugin-html'
+import viteCompression from 'vite-plugin-compression'
+import eslintPlugin from 'vite-plugin-eslint'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+// @see: https://vitejs.dev/config/
+export default defineConfig((mode: ConfigEnv): UserConfig => {
+	const env = loadEnv(mode.mode, process.cwd())
+	const viteEnv = wrapperEnv(env)
+
+	return {
+		// base: "/",
+		// alias config
+		resolve: {
+			alias: {
+				'@': resolve(__dirname, './src')
+			}
+		},
+		// global css
+		css: {
+			preprocessorOptions: {
+				less: {
+					// modifyVars: {
+					// 	"primary-color": "#1DA57A",
+					// },
+					javascriptEnabled: true,
+					additionalData: `@import "@/styles/var.less";`
+				}
+			}
+		},
+		plugins: [
+			react(),
+			createHtmlPlugin({
+				inject: {
+					data: {
+						title: viteEnv.VITE_GLOB_APP_TITLE
+					}
+				}
+			}),
+			// * EsLint 报错信息显示在浏览器界面上
+			eslintPlugin({
+				cache: false,
+				useEslintrc: true
+			}),
+			// * gzip compress
+			viteEnv.VITE_BUILD_GZIP &&
+				viteCompression({
+					verbose: true,
+					disable: false,
+					threshold: 10240,
+					algorithm: 'gzip',
+					ext: '.gz'
+				})
+		],
+		esbuild: {
+			pure: viteEnv.VITE_DROP_CONSOLE ? ['console.log', 'debugger'] : []
+		},
+		build: {
+			outDir: 'dist',
+			// esbuild 打包更快，但是不能去除 console.log，去除 console 使用 terser 模式
+			minify: 'esbuild',
+			// minify: "terser",
+			// terserOptions: {
+			// 	compress: {
+			// 		drop_console: viteEnv.VITE_DROP_CONSOLE,
+			// 		drop_debugger: true
+			// 	}
+			// },
+			rollupOptions: {
+				output: {
+					// Static resource classification and packaging
+					chunkFileNames: 'assets/js/[name]-[hash].js',
+					entryFileNames: 'assets/js/[name]-[hash].js',
+					assetFileNames: 'assets/[ext]/[name]-[hash].[ext]',
+					exports: 'default'
+				}
+			}
+		}
+	}
 })
